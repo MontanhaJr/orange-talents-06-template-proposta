6126c41fd45e6ab4d249deb216ae6057a304b78d
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-07-29 15:13:35 -0300

    Desabilitar Spring Security no ambiente dev

--- src/main/java/br/com/zupacademy/mauricio/desafioproposta/config/oauth2/DevSecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/mauricio/desafioproposta/config/oauth2/DevSecurityConfiguration.java
@@ -9,25 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-@Profile("prod")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
         http.authorizeRequests(authorizeRequest ->
                 authorizeRequest
-                        .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_propostas:read")
-                        .antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:read")
-                        .antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:write")
-                        .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_propostas:write")
-                        .antMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
+                        .antMatchers("/**").permitAll()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2-console/**");
+        web.ignoring().antMatchers("/**");
     }
 }
